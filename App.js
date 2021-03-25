8713cdbf711fdf398fabcec619bca18a66bd847f
Steven Condor <stcondor12@gmail.com>
2021-03-24 18:32:53 -0700

    Help button added and alignment modified

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import FirebaseUtils from './components/FirebaseUtils'
-import RestaurantList from './components/RestaurantList'
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import FirebaseUtils from './components/FirebaseUtils';
+import RestaurantList from './components/RestaurantList';
+import HelpButton from './components/HelpButton';
 
 export default function App() {
   FirebaseUtils.init();
   return (
     <View style={styles.container}>
+      <HelpButton></HelpButton>
       <Text>Open up App.js to start working on your app!</Text>
-      <RestaurantList></RestaurantList>
+      <RestaurantList style={styles.list}></RestaurantList>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,7 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: Platform.OS === 'android' ? 20 : 0,
   },
 });
