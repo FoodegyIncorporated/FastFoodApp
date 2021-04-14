fd98b622ebdd473eaaeb84386ec5a174c993acd2
Data <bridgecptn@gmail.com>
2021-04-14 14:35:29 -0700

    Add React Navigation

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
