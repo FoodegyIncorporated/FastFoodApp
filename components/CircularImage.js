a09524017489347f0a2625cb23dbd6eb6955b49a
Data <bridgecptn@gmail.com>
2021-04-18 18:48:09 -0700

    Vertically center content in CircularImage

--- components/CircularImage.js
+++ components/CircularImage.js
@@ -25,6 +25,7 @@ const styles = StyleSheet.create({
     container :{
         flex: 1,
         alignItems: 'center',
+        justifyContent: 'center',
     },
     original: {
         position: 'absolute',
