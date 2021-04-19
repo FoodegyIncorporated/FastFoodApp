8257baa381c3b07cd16b7a43616dc7d8a14447af
Data <bridgecptn@gmail.com>
2021-04-18 18:49:10 -0700

    LoginButton: Use single style for buttons. Add logo and title text. Style subtitle. Add function to handle guest button and go to Main screen.

--- components/LoginButton.js
+++ components/LoginButton.js
@@ -1,55 +1,80 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { ImageBackground, StyleSheet, View, Image, Text, Button } from 'react-native';
 
-function loginButton(props) {
+function loginButton({navigation}) {
     const handlePress = () => console.log("Button Pressed");
+    const handleGuest = () => {
+        navigation.navigate('Main');
+    };
     return (
-        <View
-        style = {styles.background}
-        >
-
+        <SafeAreaView style={styles.container}>
+            <ImageBackground 
+            source={require("../assets/uiForApp/ScreenForComponents.png")} 
+            style={styles.background}
+            >
+            <View style={styles.titles}>
+                <Text style={styles.title}>Foodegy</Text>
+            </View>
             <View style={styles.logoContainer}>
-            <Image style = {styles.logo} source = {require("../assets/favicon.png")}/>
-            <Text>Choose What To Eat With Ease</Text>
+                <Image style = {styles.logo} source = {require("../assets/uiForApp/logoForApp.png")}/>
+                <Text style={styles.subtitle}>Choose What To Eat With Ease</Text>
             </View>
-            
-            <View style = {styles.loginButton}>
-             <Button title = "Log In" color = "white" onPress = {handlePress}>
-             </Button>
+            <View style={styles.footer}>
+                <View style = {styles.bottomButton}>
+                    <Button title="Log In" color="#fc5c65" onPress={handlePress} />
+                </View>
+                <View style = {styles.bottomButton}>
+                    <Button title="Register" color="#43cdc4" onPress={handlePress} />
+                </View>
+                <View style = {styles.bottomButton}>
+                    <Button title="Continue as Guest" color="#a0a0a0" onPress={handleGuest} />
+                </View>
             </View>
-             
-            <View style = {styles.registerButton}>
-                <Button color = "white" title = "Register" onPress = {handlePress}/>
-            </View>
-            
-        </View>
+            </ImageBackground>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create ({
+    container: {
+        flex: 1,
+    },
     background: {
+        flex: 1,
+    },
+    titles: {
+        flex: 2,
+        width: '100%',
+        justifyContent: 'flex-end',
+    },
+    title: {
+        fontSize: 50,
+        fontWeight: '500',
+        color: '#fff',
+        textAlign: "center",
+    },
+    logoContainer:{
+        flex: 5,
+        justifyContent: 'center',
+        alignItems:"center",
+    },
+    logo: {
+        height: 150,
+        width: 150,
+    },
+    subtitle: {
+        fontSize: 24,
+        color: 'white',
+    },
+    footer: {
         flex: 1,
         justifyContent: "flex-end",
         alignItems: "center",
-    },
-    loginButton:{
         width: "100%",
-        height: 70,
-        backgroundColor:"#fc5c65",
     },
-    logo: {
-        height: 100,
-        width:100,
-    },
-    logoContainer:{
-        position:"absolute",
-        top:70,
-        alignItems:"center",
-    },
-    registerButton:{
+    bottomButton:{
         width: "100%",
-        height: 70,
-        backgroundColor:"#43cdc4",
     },
 });
 
